c4dd5f2cf68c4dde8e55ef69781d2ea2bf89dafb
César Augusto Pinardi <cesar.pinardi@gmail.com>
2018-08-23 11:19:49 -0300

    Update MIPS.s

--- MIPS.s
+++ MIPS.s
@@ -1,28 +1,35 @@
 # PROJETO DE INICIO DE ARQUITETURA
-# versao 1.0 17/08
+# versao pre-alfa 23/08
 
-# Adriano de Ovo Munin  xX420blazeitniggalol666Xx
-# Fabio Seiji Irokawa   17057720
-# Cesar Augusto Pinardi 17270182
-# Lucas Coutinho        17776501
-# Fabio Luis Dumont     17049461
+# Adriano de Oliveira Munin  	17066960
+# Fabio Seiji Irokawa   	17057720
+# Cesar Augusto Pinardi 	17270182
+# Lucas Coutinho        	17776501
+# Fabio Luis Dumont     	17049461
 
 
 .data
 
-	msgInicio:  	.asciiz "PROJETO ARQUITETURA DE COMPUTADORES:\n\nCONTROLE DE ABASTECIMENTO\n"
-	msgDev:				.asciiz "\nDesenvolvido por:\n Adriano\n Cesar\n Fabio Seiji\n Lucas\n Fabio Dumont\n"
-
+	msgInicio:  		.asciiz "PROJETO ARQUITETURA DE COMPUTADORES:\n\nCONTROLE DE ABASTECIMENTO\n"
+	cadastro: 		.space 4800 			#48b por cadastro 
+	msgMenu: 		.asciiz "\n Menu:\n 1) Cadastro \n 2) Excluir abastecimento \n 3) Exibir abastecimento \n 4) Exibir consumo medio \n 5) Exibir preco medio por posto \n 6) Creditos \n 0) Sair \n"
+	msgCadastroData:	.asciiz "\n Insira uma data (dd/mm/aa): "
+	msgCadastroPosto:	.asciiz	"\n Insira o nome do posto (minusculo): "
+	msgCadastroKm:		.asciiz	"\n Insira a km atual do veiculo: "
+	msgCadastroQtd:		.asciiz "\n Insira a quantidade de combustivel: "
+	msgCadastroPreco:	.asciiz	"\n Insira o preco por litro (ex: R$3,99/L = 3.99): "	
+	
 .text
 main:
 	#mensagens de introdução
-	li $v0, 4								# Codigo SysCall p/ escrever strings
+	li $v0, 4					# Codigo SysCall p/ escrever strings
 	la $a0, msgInicio				# Parametro (string a ser escrita)
 	syscall
-
+	
 	li $v0, 4
-	la $a0, msgDev
+	la $a0, msgMenu
 	syscall
+
 
 exit:
 	#avisar que acabou
